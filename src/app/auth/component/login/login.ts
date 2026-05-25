c2dcca263bcf2b2533c78c4c205cec32ea3b3a7b
Cristhian Camacho <cristhian.camacho@gulfstream.com>
2026-05-25 16:39:59 -0700

    login page, register page finished design with error hints,

--- src/app/auth/component/login/login.ts
+++ src/app/auth/component/login/login.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { debounce, email, form, FormField, minLength, required } from '@angular/forms/signals';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+
+  imports: [
+    FormField,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    RouterLink,
+  ],
+
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginModel = signal<LoginData>({
+    email: '',
+    password: '',
+  });
 
+  loginForm = form(this.loginModel, (schemaPath) => {
+    debounce(schemaPath.email, 500);
+    required(schemaPath.email, { message: 'Email is required' });
+    email(schemaPath.email);
+    required(schemaPath.password, { message: 'Password is required' });
+  });
+
+  onSubmit(event: Event) {
+    const credentials = this.loginModel();
+    console.log('Login:', credentials);
+    event.preventDefault();
+  }
 }
--- src/app/auth/component/register/register.ts
+++ src/app/auth/component/register/register.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { form, FormField, required, email, minLength } from '@angular/forms/signals';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormField, MatCardModule, MatInputModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  registerModel = signal<RegisterData>({
+    name: '',
+    email: '',
+    password: '',
+  });
 
+  registerForm = form(this.registerModel, (path) => {
+    // NAME
+    required(path.name, { message: 'Name is required' });
+    minLength(path.name, 3, {
+      message: 'Name must be at least 3 characters',
+    });
+
+    // EMAIL
+    required(path.email, { message: 'Email is required' });
+    email(path.email, { message: 'Enter a valid email address' });
+
+    // PASSWORD
+    required(path.password, { message: 'Password is required' });
+    minLength(path.password, 8, {
+      message: 'Password must be at least 8 characters',
+    });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const data = this.registerModel();
+    console.log('Register:', data);
+  }
 }
